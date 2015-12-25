1bea07a01e95010838e696745e954c11f5361280
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-12-25 18:35:36 -0500

    Implement redirects, ensure files end with newline.

--- utils/export-from-motor-blog.py
+++ utils/export-from-motor-blog.py
@@ -12,21 +12,16 @@ blog_dir = os.path.normpath(os.path.join(
 
 urls = re.compile(r'https?://(emptysquare.net|emptysqua.re)/')
 
-for post in db.posts.find({'pub_date': {'$exists': True}}):
-    slug = post['slug']
-    post_dir = os.path.join(blog_dir, slug)
-    shutil.rmtree(post_dir, ignore_errors=True)
-    os.mkdir(post_dir)
-    post_path = os.path.join(post_dir, 'contents.lr')
-    post['tags'] = '\n'.join(post.get('tags', []))
-    post['categories'] = '\n'.join(
-        c['name'] for c in post.get('categories', []))
-    post['summary'] = post.get('meta_description', post['summary'])
-    post['original'] = urls.sub('/', post['original'])
 
-    with open(post_path, 'w') as contents:
-        print(post['title'])
-        contents.write(u'''_model: blog-post
+def render_post(p, f):
+    p['tags'] = '\n'.join(p.get('tags', []))
+    p['categories'] = '\n'.join(
+        c['name'] for c in p.get('categories', []))
+    p['summary'] = p.get('meta_description', p['summary'])
+    p['original'] = urls.sub('/', p['original'])
+
+    print(p['title'])
+    f.write(u'''_model: blog-post
 ---
 title: {title}
 ---
@@ -48,4 +43,27 @@ summary: {summary}
 ---
 body:
 
-{original}'''.format(**post).encode(errors='ignore'))
+{original}
+'''.format(**p).encode(errors='ignore'))
+
+
+def render_redirect(p, f):
+    f.write(u'''_model: redirect
+---
+target: /blog/{redirect}
+'''.format(**p).encode(errors='ignore'))
+
+
+for post in db.posts.find({'pub_date': {'$exists': True}}):
+    slug = post['slug']
+    post_dir = os.path.join(blog_dir, slug)
+    shutil.rmtree(post_dir, ignore_errors=True)
+    os.mkdir(post_dir)
+    post_path = os.path.join(post_dir, 'contents.lr')
+
+    with open(post_path, 'w') as contents:
+        if post['type'] in ('page', 'post'):
+            render_post(post, contents)
+        else:
+            assert post['type'] == 'redirect'
+            render_redirect(post, contents)
