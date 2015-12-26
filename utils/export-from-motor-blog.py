c84ac23dc2b3c81a87e81f5b48e18ed228de3b8a
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-12-26 10:18:43 -0500

    Encoding and Markdown-style image fixes.

--- utils/export-from-motor-blog.py
+++ utils/export-from-motor-blog.py
@@ -20,36 +20,44 @@ urls = re.compile(
 
 imgs = re.compile(r'<img(?P<pre>\s.*?)src="/blog/media/'
                   r'(?P<path>(.*?)(/|%2F)(?P<name>[^"/]+))"'
-                  r'(?P<post>.*?)/?>')
+                  r'(?P<post>.*?)/?>', re.S)
 
+# Like ![title](/blog/media/2015/12/26/foo.jpg "optional alt text")
+# I ignore the alt text since I didn't put good alts in on Motor-Blog.
 md_imgs = re.compile(r'!\[(?P<title>.*?)]\(/blog/media/'
-                     r'(?P<path>([^)]+)(/|%2F)(?P<name>[^(]+?))(\s+"(.*?)")?\)')
+                     r'(?P<path>([^)]+)(/|%2F)(?P<name>[^(\]\s]+?))'
+                     r'(\s+"(.*?)")?\)', re.S)
 
 
 def replace_img(post_dir, match):
-    path = match.group('path')
-    name = match.group('name')
+    # Before and after the image path.
+    pre = match.group('pre')
+    post = match.group('post')
 
     # I once quoted a few paths by accident with Motor-Blog.
-    gridout = gfs.get_last_version(urllib.unquote(path))
+    path = urllib.unquote(match.group('path'))
+    name = urllib.unquote(match.group('name')).split('/')[-1]
+
+    gridout = gfs.get_last_version(path)
     with open(os.path.join(post_dir, name), 'wb') as f:
         f.write(gridout.read())
 
-    return match.expand(r'<img\g<pre>src="\g<name>"\g<post>/>')
+    return u'<img{pre}src="{name}"{post}/>'.format(**locals())
 
 
 def replace_md_img(post_dir, match):
-    title = match.group('title')
-    path = match.group('path')
-    name = match.group('name')
+    title = match.group('title').replace('\n', ' ')
 
     # I once quoted a few paths by accident with Motor-Blog.
-    gridout = gfs.get_last_version(urllib.unquote(path))
+    path = urllib.unquote(match.group('path'))
+    name = urllib.unquote(match.group('name'))
+
+    gridout = gfs.get_last_version(path)
     with open(os.path.join(post_dir, name), 'wb') as f:
         f.write(gridout.read())
 
-    return ('<img style="display:block; margin-left:auto; margin-right:auto;" '
-            'src="{name}" title="{title}" />').format(**locals())
+    return (u'<img style="display:block; margin-left:auto; margin-right:auto;" '
+            u'src="{name}" title="{title}" />').format(**locals())
 
 
 def render_post(p, post_dir, f):
@@ -88,14 +96,14 @@ summary: {summary}
 body:
 
 {original}
-'''.format(**p).encode(errors='ignore'))
+'''.format(**p).encode('utf-8', errors='ignore'))
 
 
 def render_redirect(p, _, f):
     f.write(u'''_model: redirect
 ---
 target: /blog/{redirect}
-'''.format(**p).encode(errors='ignore'))
+'''.format(**p).encode('utf-8', errors='ignore'))
 
 
 def main():
