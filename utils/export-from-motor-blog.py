b1ac9a6e777600092a8e55a85fde63c20da561bd
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-12-25 21:13:13 -0500

    Export images from Motor-Blog.

--- utils/export-from-motor-blog.py
+++ utils/export-from-motor-blog.py
@@ -2,25 +2,49 @@ import os
 import re
 import shutil
 import sys
+import urllib
+from functools import partial
 
+import gridfs
 import pymongo
 
 db = pymongo.MongoClient().motorblog
+gfs = gridfs.GridFS(db)
+
 this_dir = os.path.dirname(sys.argv[0])
 blog_dir = os.path.normpath(os.path.join(
     this_dir, '..', 'emptysquare', 'content', 'blog'))
 
-urls = re.compile(r'https?://(emptysquare.net|emptysqua.re)/')
+urls = re.compile(
+    r'https?://(emptysquare.net|emptysqua.re)/')
+
+imgs = re.compile(r'<img(?P<pre>\s.*?)src="/blog/media/'
+                  r'(?P<path>(.*?)(/|%2F)(?P<name>[^"/]+))"'
+                  r'(?P<post>.*?)/?>')
 
 
-def render_post(p, f):
+def replace_img(post_dir, match):
+    path = match.group('path')
+    name = match.group('name')
+
+    # I once quoted a few paths by accident with Motor-Blog.
+    gridout = gfs.get_last_version(urllib.unquote(path))
+    with open(os.path.join(post_dir, name), 'wb') as f:
+        f.write(gridout.read())
+
+    return match.expand(r'<img\g<pre>src="\g<name>"\g<post>/>')
+
+
+def render_post(p, post_dir, f):
+    print(p['title'])
+
     p['tags'] = '\n'.join(p.get('tags', []))
     p['categories'] = '\n'.join(
         c['name'] for c in p.get('categories', []))
     p['summary'] = p.get('meta_description', p['summary'])
     p['original'] = urls.sub('/', p['original'])
+    p['original'] = imgs.sub(partial(replace_img, post_dir), p['original'])
 
-    print(p['title'])
     f.write(u'''_model: blog-post
 ---
 title: {title}
@@ -47,23 +71,27 @@ body:
 '''.format(**p).encode(errors='ignore'))
 
 
-def render_redirect(p, f):
+def render_redirect(p, _, f):
     f.write(u'''_model: redirect
 ---
 target: /blog/{redirect}
 '''.format(**p).encode(errors='ignore'))
 
 
-for post in db.posts.find({'pub_date': {'$exists': True}}):
-    slug = post['slug']
-    post_dir = os.path.join(blog_dir, slug)
-    shutil.rmtree(post_dir, ignore_errors=True)
-    os.mkdir(post_dir)
-    post_path = os.path.join(post_dir, 'contents.lr')
+def main():
+    for p in db.posts.find({'pub_date': {'$exists': True}}):
+        slug = p['slug']
+        post_dir = os.path.join(blog_dir, slug)
+        shutil.rmtree(post_dir, ignore_errors=True)
+        os.mkdir(post_dir)
+        post_path = os.path.join(post_dir, 'contents.lr')
 
-    with open(post_path, 'w') as contents:
-        if post['type'] in ('page', 'post'):
-            render_post(post, contents)
-        else:
-            assert post['type'] == 'redirect'
-            render_redirect(post, contents)
+        with open(post_path, 'w') as contents:
+            if p['type'] in ('page', 'post'):
+                render_post(p, post_dir, contents)
+            else:
+                assert p['type'] == 'redirect'
+                render_redirect(p, post_dir, contents)
+
+if __name__ == '__main__':
+    main()
