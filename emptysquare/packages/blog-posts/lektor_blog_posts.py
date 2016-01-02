8bc1102de69a08fae5c0590a3e446f7d3ca0f7e3
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-01-01 22:03:13 -0500

    Add date and author to article head.

--- emptysquare/packages/blog-posts/lektor_blog_posts.py
+++ emptysquare/packages/blog-posts/lektor_blog_posts.py
@@ -9,9 +9,15 @@ def blog_posts():
     return ctx.record.pagination.items.filter(F._model == 'blog-post' and F.type == 'post')
 
 
+def pub_date(dt):
+    return '%s %s, %s' % (dt.strftime('%b'), dt.day, dt.year)
+
+
 class BlogPostsPlugin(Plugin):
     name = u'blog-posts'
     description = u'Just for emptysqua.re: filter pagination of blog posts.'
 
     def on_setup_env(self, **extra):
-        self.env.jinja_env.globals.update(blog_posts=blog_posts)
+        jinja_env = self.env.jinja_env
+        jinja_env.globals['blog_posts'] = blog_posts
+        jinja_env.filters['pub_date'] = pub_date
