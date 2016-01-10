9d53206f794d8c6c96b404d870fba818e6a6b85b
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-01-10 02:21:08 -0500

    Spin off my lektor-tags plugin.

--- emptysquare/packages/blog-posts/lektor_blog_posts.py
+++ emptysquare/packages/blog-posts/lektor_blog_posts.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
-import posixpath
-
-from jinja2 import Undefined
-from lektor.build_programs import BuildProgram
 from lektor.context import get_ctx
 from lektor.db import F
 from lektor.pluginsystem import Plugin
-from lektor.sourceobj import VirtualSourceObject
 from lektor.types import Type
 import markdown  # This is "Python Markdown": pip install markdown
-from werkzeug.utils import cached_property
 
 
 def blog_posts():
@@ -35,73 +29,6 @@ class MotorBlogMarkdownType(Type):
             'toc']))
 
 
-class TagPage(VirtualSourceObject):
-    def __init__(self, parent, plugin, tag):
-        if not tag:
-            raise ValueError("invalid tag %r" % tag)
-        VirtualSourceObject.__init__(self, parent)
-        self.plugin = plugin
-        self.tag = tag
-
-    @cached_property
-    def items(self):
-        if self.tag is None:
-            return []
-        return list(self._iter_items())
-
-    def _iter_items(self):
-        model_id = self.plugin.get_tag_model()
-        for item in self.parent.children:
-            if item.datamodel.id != model_id:
-                continue
-
-            tags = self.plugin.get_tags(item)
-            if tags and self.tag in tags:
-                yield item
-
-    @property
-    def path(self):
-        return self._get_path(self.parent.path)
-
-    @property
-    def url_path(self):
-        return self._get_path(self.parent.url_path)
-
-    def _get_path(self, root):
-        pieces = []
-        root = root.strip('/')
-        if root:
-            pieces.append(root)
-
-        pieces.append(self.plugin.get_tag_path_root())
-        if self.tag is not None:
-            pieces.append(self.tag)
-
-        return '/%s/' % '/'.join(pieces)
-
-    @property
-    def template_name(self):
-        return self.plugin.get_template_name()
-
-
-class TagPageBuildProgram(BuildProgram):
-    def produce_artifacts(self):
-        self.declare_artifact(
-            posixpath.join(self.source.url_path, 'index.html'),
-            sources=list(self.source.iter_source_filenames()))
-
-    def build_artifact(self, artifact):
-        artifact.render_template_into(self.source.template_name,
-                                      this=self.source)
-
-
-def get_path_segments(str):
-    pieces = str.split('/')
-    if pieces == ['']:
-        return []
-    return pieces
-
-
 class BlogPostsPlugin(Plugin):
     name = u'blog-posts'
     description = u'Lektor customization just for emptysqua.re.'
@@ -112,35 +39,3 @@ class BlogPostsPlugin(Plugin):
 
     def get_blog_path(self):
         return self.get_config().get('blog_path', '/blog')
-
-    def get_tag_path_root(self):
-        return self.get_config().get('tag_path_root', 'tag').strip('/')
-
-    def get_tag_model(self):
-        return self.get_config().get('tag_model', 'blog-post')
-
-    def get_tags(self, record):
-        return record[self.get_tag_field_name()]
-
-    def get_tag_field_name(self):
-        return self.get_config().get('tag_field', 'tags')
-
-    def get_template_name(self):
-        return self.get_config().get('template', 'tag.html')
-
-    def get_all_tags(self, blog):
-        tag_field = self.get_tag_field_name()
-        model_id = self.get_tag_model()
-        tags = set()
-        for item in blog.children:
-            if item.datamodel.id != model_id:
-                continue
-            if tag_field not in item:
-                continue
-            item_tags = item[tag_field]
-            if isinstance(item_tags, (list, tuple)):
-                tags |= set(item_tags)
-            elif not isinstance(item_tags, Undefined):
-                tags.add(item_tags)
-
-        return sorted(tags)
